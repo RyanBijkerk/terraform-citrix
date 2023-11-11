789882ca20f6ed1e54cab031e0553b2e81e11db4
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2023-11-11 09:20:00 +0100

    Added initial provider config with variables

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~1.6.0"
+
+  required_providers {
+    citrix = {
+      source = "citrix/citrix"
+      version = "0.3.2"
+    }
+  }
+}
+
+provider "citrix" {
+  customer_id   = var.customer_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "customer_id" {
+  type = string
+  description = "The Citrix Cloud customer id"
+}
+
+variable "client_id" {
+  type = string
+  description = "The Citrix Cloud client id"
+}
+
+variable "client_secret" {
+  type = string
+  description = "The Citrix Cloud client secret"
+}
