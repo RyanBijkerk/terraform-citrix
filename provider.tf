4cf7e6ecccbd2dc0abbac625a93a79c3e2a8ab1a
Ryan Ververs-Bijkerk <ryan@logitblog.com>
2023-11-11 12:02:17 +0100

    Update in the provider and varibales

--- provider.tf
+++ provider.tf
@@ -2,10 +2,29 @@ terraform {
   required_version = "~> 1.6.0"
 
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.80.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.5.1"
+    }
+
     citrix = {
       source  = "citrix/citrix"
       version = ">= 0.3.2"
     }
+  }
+}
+provider "azurerm" {
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id       = var.azure_application_id
+  client_secret   = var.azure_application_secret
+
+  features {
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -12,3 +12,47 @@ variable "client_secret" {
   type        = string
   description = "The Citrix Cloud client secret"
 }
+
+variable "name" {
+  type        = string
+  default     = "GO"
+  description = "The name that will be used to create the resources"
+}
+
+variable "domain" {
+  type        = string
+  description = "The fqdn domain name"
+  default     = "go.euc"
+}
+
+variable "location" {
+  type        = string
+  description = "The Azure location"
+  default     = "West Europe"
+}
+
+variable "sku" {
+  type        = string
+  description = "The Azure SKU type for the master and Citrix machine catalog"
+  default     = "Standard_D4s_v5"
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "The Azure Tenant ID"
+}
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "The Azure Subscription ID"
+}
+
+variable "azure_application_secret" {
+  type        = string
+  description = "The Azure Application Secret"
+}
+
+variable "azure_application_id" {
+  type        = string
+  description = "The Azure Application ID"
+}
